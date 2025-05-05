866dee3ef1c4db34b33a8d5b73e16706ee2b33ea
nestor1998 <nestorignacioalarcon@gmail.com>
2025-05-05 16:29:15 -0400

    fecha agregada

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
 
     const grade = document.getElementById("grade").value.trim();
 
+    const date = document.getElementById("date").value.trim();
+
     if(grade <1 || grade>7 || !name || !lastName || isNaN(grade) ){
         alert("Error Datos Incorrectos")
         return
@@ -19,7 +21,7 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
 
     //guardar datos en el Array
 
-    const student ={name,lastName,grade};
+    const student ={name,lastName,grade,date};
     students.push(student);
 
     addStudentToTable(student)
@@ -35,7 +37,9 @@ function addStudentToTable(student){
     row.innerHTML=
     `<td>${student.name} </td>
     <td>${student.lastName} </td>
-    <td>${student.grade} </td>`;
+    <td>${student.grade} </td>
+    <td>${student.date} </td>`
+    ;
     tableBody.appendChild(row);
 }
 
